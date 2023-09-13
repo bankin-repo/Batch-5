3e56b59cae6fc00a728cbcbaff310bb4db94e632
ELPDevOps <139422116+ELPDevOps@users.noreply.github.com>
2023-09-13 22:30:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-TOMCAT_HOST=54.226.165.119
+TOMCAT_HOST=44.217.192.242
 TOMCAT_USER=tomcat
-TOMCAT_PWD=12345678
-TOMCAT_HOME=/opt/apache-tomcat-9.0.78
-#
-WAR_FILE_PATH=./target/ELPDevOps.war
+TOMCAT_HOME=/opt/tomcat
+WAR_FILE_PATH=./target/*.war
 
-echo "Bringing tomcat server down for deployment..."
-sleep 3
-
-$TOMCAT_HOME/bin/shutdown.sh 1>/dev/null 2>&1
-
-#1>file.log 2>&1
-if [ -f $WAR_FILE_PATH ];then
-    echo "deploying ELPDevOps application..."
-    sleep 4
-    scp  $WAR_FILE_PATH $TOMCAT_USER@$TOMCAT_HOST:$TOMCAT_HOME/webapps
+if [ -f $WAR_FILE_PATH ]; then
+	echo "Bringing tomcat server down for deployment..."
+	ssh $TOMCAT_USER@$TOMCAT_HOST "$TOMCAT_HOME/bin/shutdown.sh"
+    echo "Deploying ELPDevOps application..."
+    scp $WAR_FILE_PATH $TOMCAT_USER@$TOMCAT_HOST:$TOMCAT_HOME/webapp
+	echo "Starting tomcat server..."
+	ssh $TOMCAT_USER@$TOMCAT_HOST "$TOMCAT_HOME/bin/startup.sh"
+	echo "Deployment is successful! Email notification has been sent!"
 else
-   echo "war file doesn't exist! Please check the build"
+    echo "WAR file doesn't exist! Please check the build."
 fi
-
-echo "starting tomcat server..."
-$TOMCAT_HOME/bin/startup.sh 1>/dev/null 2>&1
-
-echo "deployment is successful! Email notification has been sent!"
