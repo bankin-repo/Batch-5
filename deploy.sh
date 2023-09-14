22da421be1f17d1cff6ff8f9b2c84ad901fcefcf
ELPDevOps <139422116+ELPDevOps@users.noreply.github.com>
2023-09-14 21:58:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ if [ -f $WAR_FILE_PATH ]; then
 	echo "Bringing tomcat server down for deployment..."
 	ssh $TOMCAT_USER@$TOMCAT_HOST "$TOMCAT_HOME/bin/shutdown.sh"
     echo "Deploying ELPDevOps application..."
-    scp $WAR_FILE_PATH $TOMCAT_USER@$TOMCAT_HOST:$TOMCAT_HOME/webapp
+    scp $WAR_FILE_PATH $TOMCAT_USER@$TOMCAT_HOST:$TOMCAT_HOME/webapps/
 	echo "Starting tomcat server..."
 	ssh $TOMCAT_USER@$TOMCAT_HOST "$TOMCAT_HOME/bin/startup.sh"
 	echo "Deployment is successful! Email notification has been sent!"
